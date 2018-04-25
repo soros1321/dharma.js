5c115d5d1ee3f7732c990838bdb99fa516d73c5b
Christopher Min <chris@medisas.com>
2018-04-24 19:08:40 -0700

    feat: add tokenAPI to collateralized simple interest loan adapter

--- src/apis/adapters_api.ts
+++ src/apis/adapters_api.ts
@@ -3,7 +3,7 @@ import * as singleLineString from "single-line-string";
 import * as Web3 from "web3";
 
 // APIs
-import { ContractsAPI } from "./";
+import { ContractsAPI, TokenAPI } from "./";
 
 // Adapters
 import {
@@ -49,14 +49,17 @@ export class AdaptersAPI {
     public collateralizedSimpleInterestLoan: CollateralizedSimpleInterestLoanAdapter;
 
     private contracts: ContractsAPI;
+    private token: TokenAPI;
 
-    constructor(web3: Web3, contractsApi: ContractsAPI) {
+    constructor(web3: Web3, contractsApi: ContractsAPI, tokenApi: TokenAPI) {
         this.contracts = contractsApi;
+        this.token = tokenApi;
 
         this.simpleInterestLoan = new SimpleInterestLoanAdapter(web3, this.contracts);
         this.collateralizedSimpleInterestLoan = new CollateralizedSimpleInterestLoanAdapter(
             web3,
             this.contracts,
+            this.token,
         );
     }
 
